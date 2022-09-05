66801103d505dcf8a7161637187c35fa13f7d3bb
Kato La <56576057+laviktor@users.noreply.github.com>
2022-09-05 19:28:20 -0400

    Add files via upload

---
+++ test/test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  test
+//
+//  Created by ラ・カトー on 9/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+//  MARK: - Section 2
+//  Used to to create a new "section" for the above tab
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
